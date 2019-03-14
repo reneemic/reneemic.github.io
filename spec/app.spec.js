96189e387ee20ccb4bd7b8c5011ddfb193ea7f89
reneemic <reneemic75@gmail.com>
2019-03-14 16:09:32 -0400

    updated testing docs

---
+++ spec/app.spec.js
@@ -0,0 +1,22 @@
+describe("Open on Click", function() {
+  var modal;
+  it(btn.onclick, function() {
+  	expect(modal.style.display = "block");
+})
+
+
+describe("Click Outside", function() {
+  var modal;
+  it(window.onclick = function(event) {
+  	expect(if (event.target == modal
+  	 modal.style.display = "none"));
+ })
+
+ }) 
+
+  
+  
+
+    // describe('someOtherFunction', function() {
+    //     // tests about `someOtherFunction()`
+    // })
